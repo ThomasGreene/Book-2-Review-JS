1e9ed096c23a20dc2fc6065736ef65caefc4a5f8
Joe Shepherd <joeshepmedia@gmail.com>
2019-06-10 10:25:34 -0500

    Begin modularization

---
+++ src/scripts/api.js
@@ -0,0 +1,26 @@
+function getMyFood() {
+  fetch("http://localhost:8088/food")
+  .then( data => data.json())
+  .then( foodData => {
+    console.log(foodData)
+    foodData.forEach( foodItem => {
+      fetch(`https://world.openfoodfacts.org/api/v0/product/${foodItem.barcode}.json`)
+      .then( foodFromAPI => foodFromAPI.json())
+      .then( foodItemFromAPI => {
+        if (foodItemFromAPI.product.ingredients_text) {
+          foodItem.ingredients = foodItemFromAPI.product.ingredients_text
+        } else {
+          foodItem.ingredients = "ingredients not listed"
+        }
+        document.querySelector("#food-list").innerHTML += createFoodComponent(foodItem)
+      })
+    })
+  })
+}
+
+function createFoodComponent(foodObj) {
+  return `
+    <h2>${foodObj.name}</h2>
+    <p><strong>ingredients:</strong> ${foodObj.ingredients}</p>
+  `
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,2 @@
-fetch("http://localhost:8088/food")
-.then( data => data.json())
-.then( foodData => {
-  console.log(foodData)
-  foodData.forEach( foodItem => {
-    fetch(`https://world.openfoodfacts.org/api/v0/product/${foodItem.barcode}.json`)
-    .then( foodFromAPI => foodFromAPI.json())
-    .then( foodItemFromAPI => {
-      if (foodItemFromAPI.product.ingredients_text) {
-        foodItem.ingredients = foodItemFromAPI.product.ingredients_text
-      } else {
-        foodItem.ingredients = "ingredients not listed"
-      }
-      document.querySelector("#food-list").innerHTML += createFoodComponent(foodItem)
-    })
-  })
-})
 
-function createFoodComponent(foodObj) {
-  return `
-    <h2>${foodObj.name}</h2>
-    <p><strong>ingredients:</strong> ${foodObj.ingredients}</p>
-  `
-}
+getMyFood()
